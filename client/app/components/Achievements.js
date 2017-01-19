9341d54c0a617d67c931e3150d0d3799428147d1
blakeparkinson <wizardplow@gmail.com>
2017-01-19 16:42:51 -0700

    Rm unused code + fix the space issue

--- client/app/components/Achievements.js
+++ client/app/components/Achievements.js
@@ -17,27 +17,6 @@ var Achievements = props => (
     <div className="mid-text">I've worked with Node.js, python, PHP, ruby, golang, and a little java and objective c for app development.</div>
     <div> My passion lies in front-end and design and I've used Angular, Backbone, and put emphasis on user experience when choosing front-end frameworks.</div>
     </div>
-    {/*
-    <ul>
-      <li className="col-sm-12 col-md-4 list edmodo achievements">
-        <img className="edmodo-img" src="/images/edmodo.png"/>
-        <span className="onehalf-x">Edmodo</span>
-        <div>I worked at Edmodo, where I worked on various projects from the backend API to front end landing pages</div>
-      </li>
-      <li className="col-sm-12 col-md-4 list achievements fonts">
-        <i className="fa fa-bolt three-x"></i>
-        <span className="onehalf-x">RosterBlitz</span>
-        <div>I founded
-          <a href="https://www.rosterblitz.com">RosterBlitz</a>, the premier sports quizzing website on the internet.</div>
-      </li>
-      <li className="col-sm-12 col-md-4 list achievements fonts">
-        <i className="fa fa-building three-x"></i>
-        <span className="onehalf-x">NCTI</span>
-        <div>I worked at NCTI where I built a beautiful node/angular app from scratch.</div>
-      </li>
-    </ul>
-    */}
-
   </div>
 </div>
 )
--- client/app/components/Resume.js
+++ client/app/components/Resume.js
@@ -14,26 +14,26 @@ var Resume = props => (
         <div className="col">
           <img className="edmodo-img" src={edmodoImg}/>
           <h2>
-            <a href="https://www.edmodo.com/">Edmodo</a>
+            <a href="https://www.edmodo.com/"> Edmodo </a>
           </h2>
           <div>I worked at Edmodo, one of the primary communication platforms for K-12 schools. At Edmodo, I worked on various projects from the backend API to front end landing pages. I was one of the highest code contributors to the main
-            <a href="https://www.edmodo.com/">Edmodo platform</a>
+            <a href="https://www.edmodo.com/"> Edmodo platform </a>
             ,worked on
-            <a href="https://snapshot.edmodo.com/">Edmodo Snapshot</a>
+            <a href="https://snapshot.edmodo.com/"> Edmodo Snapshot </a>
             (a common core mastery platform), as well as the
-            <a href="https://developers.edmodo.com/">Partner API platform</a>.</div>
+            <a href="https://developers.edmodo.com/"> Partner API platform </a>.</div>
         </div>
       </li>
       <li className="row list achievements">
         <div className="col">
           <img className="alpine-img" src={alpineImg}/>
           <h2>
-            <a href="https://alpinelaboratories.com/">Alpine Labs</a>
+            <a href="https://alpinelaboratories.com/"> Alpine Labs </a>
           </h2>
           <div>I'm the mobile developer at Alpine Labs, where we make photography tech gear. I architected, built, and designed the
-            <a href="https://itunes.apple.com/us/app/pulse-camera-control/id1093969356?mt=8">Pulse app</a>
+            <a href="https://itunes.apple.com/us/app/pulse-camera-control/id1093969356?mt=8"> Pulse app </a>
             and worked on the
-            <a href="https://itunes.apple.com/us/app/radian/id593206526?mt=8">Radian App</a>. Both apps allow you to wireless control your camera from your phone or tablet.</div>
+            <a href="https://itunes.apple.com/us/app/radian/id593206526?mt=8"> Radian App </a>. Both apps allow you to wireless control your camera from your phone or tablet.</div>
         </div>
       </li>
       <li className="row list achievements">
@@ -41,10 +41,10 @@ var Resume = props => (
 
           <img className="ncti-img" src={nctiImg}/>
           <h2>
-            <a href="https://ncti.com/">NCTI</a>
+            <a href="https://ncti.com/"> NCTI</a>
           </h2>
           <div>I also work at NCTI, where I architected, designed, and built the
-            <a href="https://amp.ncti.com/">Amp Platform</a>. With this platform, we transformed 20 years of technical debt, into a modern, responsive, and lean platform.</div>
+            <a href="https://amp.ncti.com/"> Amp Platform </a>. With this platform, we transformed 20 years of technical debt, into a modern, responsive, and lean platform.</div>
         </div>
 
       </li>
