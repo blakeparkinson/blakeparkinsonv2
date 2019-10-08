ba30e006a9ef88d6f229bd80c79354def1d915e7
blakeparkinson <wizardplow@gmail.com>
2019-10-07 22:04:53 -0600

    updates

--- client/app/components/ResumePageContainer.js
+++ client/app/components/ResumePageContainer.js
@@ -1,15 +1,13 @@
 var React = require('react')
 var ResumePage = require('./ResumePage')
 
-var previousTop
-
 var positions = [
   {
     place: 'BACKBONE PLM',
     title: 'Chief Technology Officer',
     dates: 'FEB 2017 - CURRENT',
     description:
-      'Tasked with building the Backbone 2.0 Application, using Angular 2, node.js, mongodb, paperjs. This application allows you to design and create any product you like and manage it from design to factory completion. Created a photoshop like experience for image annotations using paper.js. Responsible for both front-end, back-end, and server-side + integration testing.'
+      'Tasked with building the Backbone 2.0 Application, using Angular 2, node.js, mongodb, deepstream, golang, paperjs. This application allows you to design and create any product you like and manage it from design to factory completion. Responsible for both front-end, back-end, architecture, and server-side + integration testing. Scaled development team from 2 engineers to a team of 15 developers, QA Engineers, and product managers. Distilled complex and highly-technical information into simple language easily digestable by board members, investors, and non-technical stakeholders.'
   },
   {
     place: 'ALPINE LABS',
@@ -21,16 +19,16 @@ var positions = [
   {
     place: 'NCTI',
     title: 'Web Developer',
-    dates: 'JULY  2015 - CURRENT',
+    dates: 'JULY  2015 - JAN 2017',
     description:
-      'Built brand new application (amp-ncti.com) from the ground up using node.js, angular, couchdb in 5 months, thus turning 10+ years of technical debt into a modern web application. Responsible for full frontend implementation and design. From complex angular views to all of the images and iconography on the website.'
+      'Built brand new application (amp-ncti.com) from the ground up using node.js, angular, couchdb in 5 months, thus turning 10+ years of technical debt into a modern web application. Responsible for full frontend/backend implementation and design. From complex angular views to all of the images and iconography on the website.'
   },
   {
     place: 'EDMODO',
     title: 'Software Engineer',
     dates: 'FEB 2013 - JULY 2015',
     description:
-      'Largest contributing developer to the main Edmodo.com website (which serves millions of users) accounting for around 40% of commits for the code base. Leading PHP Developer of the API use by Edmodo.com, mobile applications, and 3rd party developers. Implemented custom tracking Javascript API which allows us to mine every user action and perform relevant data analysis. Lead frontend Engineer on the Growth team. Responsible for rolling out features that saw the user base grow from 8 million users to 50 million users during my time there.'
+      'Largest contributing developer to the main Edmodo.com website (which serves millions of users) accounting for around 40% of commits for the code base. Leading PHP(ugh) Developer of the API used by Edmodo.com, mobile applications, and 3rd party developers. Implemented custom tracking Javascript API which allows us to mine every user action and perform relevant data analysis. Lead frontend Engineer on the Growth team. Responsible for rolling out features that saw the user base grow from 5,000 users to 50 million users during my time there.'
   },
   {
     place: 'UTOPY',
